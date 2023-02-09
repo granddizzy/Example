ad9b7a82308579f8538d29b15a58c7d500692833
roman lepihov <lepihovrom@yandex.by>
2023-02-09 22:08:49 +0300

    add random

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,6 +1,7 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 
 int result = numberA+numberB;
-
 Console.WriteLine(result);
